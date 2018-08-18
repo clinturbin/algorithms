5f124924cbec9370b4792742fc5447bee45f4562
clinturbin <clinturbin@gmail.com>
2018-08-18 11:37:23 -0400

    Solution for week 8 algorithm Lattice Paths

---
+++ lattice-paths.js
@@ -0,0 +1,38 @@
+//=============================================================
+//          ALGORITHMS - FIRDAY AUGUST 17, 2018
+//============================================================
+
+//-----------------------------------------------------------
+//        Algorithm 1 - Lattice Paths
+//        
+// Starting in the top left corner of a 2×2 grid, 
+// and only being able to move to the right and down, 
+// there are exactly 6 routes to the bottom right corner.
+// 
+// How many such routes are there through a 20×20 grid? 
+// In an n x n grid?
+//-----------------------------------------------------------
+
+let makeTopRow = (columns) => {
+    let topRow = [];
+    for (let i = 0; i <= columns; i++) {
+        topRow.push(1);
+    }
+    return topRow;
+};
+
+let makeGrid = (rows, columns) => {
+    let fullGrid = [];
+    fullGrid.push(makeTopRow(columns));
+    for (let i = 1; i <= rows; i++) {
+        fullGrid.push([1]);
+        for (let j = 1; j <= columns; j++) {
+            let currentRow = fullGrid[i];
+            let numberToAdd = fullGrid[i - 1][j] + fullGrid[i][j - 1];
+            currentRow.push(numberToAdd);
+        }
+    }
+    return fullGrid[rows][columns];
+}
+
+console.log(makeGrid(20, 20));
